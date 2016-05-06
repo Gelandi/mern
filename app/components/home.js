1ad9b91089a07d72d3375f999bc1ea76349d827f
Gelandi <mike.shimp@gmail.com>
2016-05-05 23:27:03 -0700

    Following the neweden react tutorial

---
+++ app/components/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className='alert alert-info'>
+        Hello from Home Component unchanged
+      </div>
+    );
+  }
+}
+
+export default Home;
+
